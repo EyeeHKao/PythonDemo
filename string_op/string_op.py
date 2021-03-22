651051f5e06a05fca1ac2b1649755822753778c9
EyeeHKing <heye_zzz@126.com>
2021-03-22 20:14:27 +0800

    Update string_op.py

--- string_op/string_op.py
+++ string_op/string_op.py
@@ -1,1 +1,5 @@
 
+#str operate:
+s = 'xxxx,yyy;zz aa\nbb   cc\n'
+s.strip('')    #return a str which is removed the specitify char,and 
+s.spilt('')    #return a list of str, which is spilted by '',not included ''
